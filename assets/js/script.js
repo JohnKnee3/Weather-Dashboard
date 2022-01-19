6ba737f3d86e5b85e2b70f38fb558b466b0bccb4
john.a.clark3 <john.a.clark3@gmail.com>
2022-01-19 15:30:47 -0700

    Creating Elements using specific JSON data
    6.2.5
    pretty dense stuff all surrounding creating elements from all of the parsed JSON data and grabbing the specific data you need from each step of the array using a for loop.

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,3 +32,4 @@
 //6.1.6 passed in a string from the call to the function to create something that can select any user that gets passed in.
 //6.2.3 hardcoded the input form into the HTML using the provided CSS for styling
 //6.2.4 addEventListener was used to call a new function that pulled the value of what was typed and send it to the next function.
+//6.2.5 pretty dense stuff all surrounding creating elements from all of the parsed JSON data and grabbing the sepcific data you need from each step of the array using a for loop.
