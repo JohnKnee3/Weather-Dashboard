f35e14b2afb2a516daa3f3227bc2b9ade65cae74
john.a.clark3 <john.a.clark3@gmail.com>
2022-01-19 10:00:27 -0700

    added some starter notes

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+//Notes----------------------------------------------------
+
+//LOOKIN AHEAD
+
+// Use the fetch API to send requests to a server-side API
+
+// Receive and parse data in the JSON format
+
+// Use returned data to dynamically generate HTML
+
+// Handle response codes and metadata, including headers, status, and URL
+
+
+//WHAT YOU WILL LEARN
+
+// Explain the difference between a client-side API and a server-side API
+
+// Explain the client-server model and request-response pattern
+
+// Explain and implement the differences between HTTP GET requests using XMLHTTPRequest, jQuery AJAX, and the fetch API
+
+// Explain HTTP response codes and handle response metadata with fetch API
+
+// Parse JSON to dynamically generate HTML
+
+// Explain the benefits and challenges of working with asynchronous JavaScript
+
+// Explain and implement query string parameters
